c7ee0359437b068aa1f17b9da902de2dd6df4cba
marcineasygraff <105993998+marcineasygraff@users.noreply.github.com>
2026-05-06 12:11:55 +0200

    Fix Leaflet icon URLs
    Remove default icon URL handling and set custom icon URLs.

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-// FIX IKON
+// FIX IKON 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
